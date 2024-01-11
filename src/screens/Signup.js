6966167620455b7f81da53aa8dbb7d38a5689141
v-iv-ek <91026668+v-iv-ek@users.noreply.github.com>
2024-01-11 16:29:50 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -8,7 +8,7 @@ function Signup() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/createuser", {
+        const response = await fetch("https://foodproject-backend.onrender.com/api/createuser", {
             method: 'POST',
             headers:{
                 'content-Type': 'application/json'
